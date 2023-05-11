d8f82dd80490a4149e59361f00ab259fa5ad8c2e
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-05-11 12:25:27 -0300

    delete commented code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './style/index.css';
-// //import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import reactLogo from './react.svg';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
-//   <div>
-//   <h1>Hello from React application</h1>
-//   <img src={reactLogo} className="logo react" alt="React logo" />
-//   </div>
-
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoApp from './components/TodosApp.jsx';
